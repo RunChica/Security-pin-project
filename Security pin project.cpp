e68b44d8d892944eff4ae1b01d8d8e6aa09b0986
RunChica <149354728+RunChica@users.noreply.github.com>
2026-07-28 01:03:23 +0800

    Add files via upload

---
+++ Security pin project.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int inputNumber()
+{
+	int iNumber;
+	cin >> iNumber;
+	return iNumber;
+}
+
+int main()
+{
+	int Card;
+	cout << "Do the resident have security card? Press 1 if yes and 0 if no ""\n";
+	Card = inputNumber();
+	bool haveCard = (Card == 1);
+
+	if (haveCard) {
+			int Pin;
+			cout << "What is the pin number?""\n";
+			Pin = inputNumber();
+			bool(PinTrue) = (Pin == 1234);
+
+			if (PinTrue) {
+				cout << "You can enter the apartment";
+			}
+			else {
+				cout << "You can't enter the apartment";
+			}
+	}
+	else{
+		cout << "You can't enter the apartment";
+	}
+
+return 0;
+}
